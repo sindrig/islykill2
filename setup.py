a8a8e23b298a52a6cf0e590c4cd5abec7fc89a06
Sindri Guðmundsson <sindrigudmundsson@gmail.com>
2016-07-06 08:31:55 +0000

    Update version

--- setup.py
+++ setup.py
@@ -8,12 +8,12 @@ except ImportError:
 
 setup(
     name='Islykill2',
-    version='1.0.1.rc0',
+    version='1.0.1',
     description='Utility to parse and verify Islykill authentication, '
                 'using SAML 2.0',
     author='Sindri Gudmundsson',
     author_email='sindrigudmundsson@gmail.com',
-    url='http://www.irdn.is/',
+    url='https://github.com/sindrig/islykill2/',
     packages=['islykill2'],
     include_package_data=True,
     zip_safe=False,
